1fe7b53dcda4ed632bacc01f693c5ef300619efd
souloppen <arieloppenheimer@outlook.cl>
2024-12-20 22:58:40 -0300

    add:Contactos boxes

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <script setup>
-import { ref } from 'vue'
+import { reactive, ref } from 'vue'
 import { RouterView } from 'vue-router'
 import { useThemeStore } from './stores/theme.store'
 import NavComponent from './components/NavComponent.vue'
 import ChangeColorButton from './components/ChangeColorButtonComponent.vue'
 import Title from './components/TitleComponent.vue'
+import Box from './components/BoxComponent.vue'
 const themeStore = useThemeStore()
 const dialog = ref(false)
+const info = reactive([
+  { name: 'Email', text: 'arieloppenheimer@gmail.com' },
+  { name: 'Github', text: 'https://github.com/SoulOppen' },
+  { name: 'Linkedin', text: 'https://www.linkedin.com/in/ariel-oppenheimer-lewin-76b05a83/' },
+])
 const añoActual = new Date().getFullYear()
 const openDialog = () => (dialog.value = !dialog.value)
+const page = (text) => text.includes('https')
 themeStore.setDarkModeBasedOnSystemPreference()
 </script>
 
@@ -25,20 +32,30 @@ themeStore.setDarkModeBasedOnSystemPreference()
   <footer class="bg-electricBlue text-center">©{{ añoActual }} Ariel Oppenheimer</footer>
   <dialog
     v-if="dialog"
-    class="fixed inset-0 flex items-center justify-center h-screen w-full mx-auto bg-black/60 pointer-events-none"
+    class="fixed inset-0 flex items-center justify-center h-screen w-full mx-auto bg-black/60"
   >
-    <div
-      class="pointer-event-auto bg-lightBg dark:bg-darkBg text-lightText dark:text-darkText w-4/5"
-    >
-      <div class="flex justify-between items-center content-center">
+    <div class="bg-lightBg dark:bg-darkBg text-lightText dark:text-darkText w-4/5">
+      <div class="flex justify-between items-center content-center pointer-events-auto">
         <Title>Contacto</Title>
         <div
-          class="flex justify-center items-center rounded-full w-6 self-start m-2 bg-darkBg dark:bg-lightBg dark:text-lightText text-darkText pointer-events-auto cursor-pointer"
+          class="flex justify-center items-center rounded-full w-6 self-start m-2 bg-darkBg dark:bg-lightBg dark:text-lightText text-darkText cursor-pointer"
           @click="() => (dialog = false)"
         >
           X
         </div>
       </div>
+      <Box v-for="item of info" :key="item.name" :name="item.name">
+        <a
+          v-if="page(item.text)"
+          :href="item.text"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="hover:text-accentElectricBlue"
+        >
+          {{ item.text }}
+        </a>
+        <p v-else>{{ item.text }}</p>
+      </Box>
     </div>
   </dialog>
 </template>
---
+++ src/components/BoxComponent.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref, defineProps } from 'vue'
+
+const props = defineProps({
+  name: { type: String, required: true },
+})
+
+const open = ref(false)
+const setOpen = () => {
+  open.value = !open.value // Cambia el estado de `open`
+}
+</script>
+
+<template>
+  <div class="rounded-md cursor-pointer mx-6 border-2 border-slate-500 m-4 text-darkText text-xl">
+    <div @click="setOpen" class="px-2 border-2 border-slate-700 bg-slate-700">{{ name }}</div>
+    <div v-if="open" class="px-2 bg-slate-500">
+      <slot />
+    </div>
+  </div>
+</template>
